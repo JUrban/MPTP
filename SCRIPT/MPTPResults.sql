6718bcaba9cb556c7a864a9e779f7f0f6c44d2d7
mizarcvs <mizarcvs>
2004-11-14 14:51:46 +0000

    changed BLOBs to TEXTs, BLOB is now binary by default in MySQL, cannot be displayed properly in phpmyadmin

--- SCRIPT/MPTPResults.sql
+++ SCRIPT/MPTPResults.sql
@@ -13,17 +13,17 @@ CREATE TABLE probleminfo (
 
   mizar_proof_length    INT UNSIGNED,		
   direct_references_nr  INT UNSIGNED,		/* Without bg theory */
-  direct_references	BLOB,			/* List of their names */
+  direct_references	TEXT,			/* List of their names */
   bg_references_nr	INT UNSIGNED,		/* bg theory */
-  bg_references		BLOB,			/* List of their names */
+  bg_references		TEXT,			/* List of their names */
   all_references_nr	INT UNSIGNED,		/* With bg theory */
 
   conjecture_syms_nr	INT UNSIGNED,
-  conjecture_syms	BLOB,
+  conjecture_syms	TEXT,
   direct_refs_syms_nr	INT UNSIGNED,
-  direct_refs_syms	BLOB,
+  direct_refs_syms	TEXT,
   problem_syms_nr	INT UNSIGNED,
-  problem_syms		BLOB,			/* All symbols */
+  problem_syms		TEXT,			/* All symbols */
 
   INDEX  xproblem 		(problem(20)), 
   INDEX  xarticle 		(article),
@@ -72,7 +72,7 @@ CREATE TABLE proved (
 
   time_limit		INT UNSIGNED,		/* In seconds */
   memory_limit		INT UNSIGNED,		/* In kb */
-  prover_parameters	BLOB,
+  prover_parameters	TEXT,
   start_date		DATE,
   hostname		VARCHAR(255),
   machine_cpu		VARCHAR(255),
@@ -84,15 +84,15 @@ CREATE TABLE proved (
 /* used_input_flas_nr = used_dir_refs_nr + used_bg_flas_nr */
 
   used_input_flas_nr	INT UNSIGNED,
-  used_input_flas	BLOB,			/* List of their names */
+  used_input_flas	TEXT,			/* List of their names */
   used_dir_refs_nr	INT UNSIGNED,
-  used_dir_refs	BLOB,			/* List of their names */
+  used_dir_refs	TEXT,			/* List of their names */
   used_bg_flas_nr	INT UNSIGNED,
-  used_bg_flas		BLOB,			/* List of their names */
+  used_bg_flas		TEXT,			/* List of their names */
 
 /* not yet */
 --  used_syms_nr		INT UNSIGNED,
---  used_syms		BLOB,			/* Only if in the proof */
+--  used_syms		TEXT,			/* Only if in the proof */
 
   PRIMARY KEY ( id ),
   INDEX  xproblem 		(problem(20)), 
@@ -117,7 +117,7 @@ CREATE TABLE proof (
 /* id is common with the proved table */
   id		        INT NOT NULL AUTO_INCREMENT,
   problem		VARCHAR(255),	/* Just consistency check */
-  proof			MEDIUMBLOB,	/* About 17M should be enough */
+  proof			MEDIUMTEXT,	/* About 17M should be enough */
   PRIMARY KEY ( id )
 );
 
@@ -158,7 +158,7 @@ CREATE TABLE unproved (
 
   time_limit		INT UNSIGNED,		/* In seconds */
   memory_limit		INT UNSIGNED,		/* In kb */
-  prover_parameters	BLOB,
+  prover_parameters	TEXT,
   start_date		DATE,
   hostname		VARCHAR(255),
   machine_cpu		VARCHAR(255),
@@ -186,22 +186,22 @@ CREATE TABLE article_bg_info (
   bg_references_nr	INT UNSIGNED,		/* complete bg theory */
 
   bg_dco_nr		INT UNSIGNED,		/* constructor types bg */
-  bg_dco		BLOB,			/* List of their names */
+  bg_dco		TEXT,			/* List of their names */
   bg_dem_nr		INT UNSIGNED,		/* mode existence bg */
-  bg_dem		BLOB,			/* List of their names */
+  bg_dem		TEXT,			/* List of their names */
   bg_pro_nr		INT UNSIGNED,		/* properties bg */
-  bg_pro		BLOB,			/* List of their names */
+  bg_pro		TEXT,			/* List of their names */
   bg_cle_nr		INT UNSIGNED,		/* cluster existence bg */
-  bg_cle		BLOB,			/* List of their names */
+  bg_cle		TEXT,			/* List of their names */
   bg_clf_nr		INT UNSIGNED,		/* functor cluster bg */
-  bg_clf		BLOB,			/* List of their names */
+  bg_clf		TEXT,			/* List of their names */
   bg_clc_nr		INT UNSIGNED,		/* condit. cluster bg */
-  bg_clc		BLOB,			/* List of their names */
+  bg_clc		TEXT,			/* List of their names */
   bg_dre_nr		INT UNSIGNED,		/* requirements bg */
-  bg_dre		BLOB,			/* List of their names */
+  bg_dre		TEXT,			/* List of their names */
 
   bg_syms_nr		INT UNSIGNED,
-  bg_syms		BLOB,			/* All symbols */
+  bg_syms		TEXT,			/* All symbols */
 
   PRIMARY KEY ( article ),
   INDEX xbg_references_nr	(bg_references_nr),
