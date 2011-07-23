ab1b93402ee8082c27adf3263d83802b160ff46f
Josef Urban <Josef.Urban@gmail.com>
2011-07-23 09:54:36 +0200

    hol2nrs

--- SCRIPT/advisor/hol2nrs.pl
+++ SCRIPT/advisor/hol2nrs.pl
@@ -1,1 +1,25 @@
-time for i in `ls`; do echo $i; perl -e '<>; $name=<>; chop($name); $_=<>; die "bad" unless m/Inherit/; while(<>) {if(m/^[>]/) {print (" ", $name, ":", join(",",@refs), ";", join(",",@syms), "\n"); exit} elsif(m/^[ABDLTS].* +(.*)/) {push(@refs, $1) } else {m/^.* (.*)/ or die; push(@syms, $1)}}' $i >>00; done
+#!/usr/bin/perl
+
+
+# run like:
+# time for i in *.prf; do echo $i; hol2nrs $i >> 00; done
+#
+
+<>; $name=<>; chop($name);
+$_=<>; die "bad" unless m/^>Inherit/;
+while(<>)
+{
+    if(m/^[>]/)
+    {
+	print (" ", $name, ":", join(",",@refs), ";", join(",",@syms), "\n");
+	exit;
+    }
+    elsif(m/^[ABDLTS].* +(.*)/)
+    {
+	push(@refs, $1);
+    }
+    else
+    {
+	m/^.* (.*)/ or die; push(@syms, $1)
+    }
+}
