994bcce823e99db74e2f752c76e737d2eb14e80e
Josef Urban <Josef.Urban@gmail.com>
2011-07-24 11:57:34 +0200

    changing to hashes

--- SCRIPT/advisor/hol2nrs.pl
+++ SCRIPT/advisor/hol2nrs.pl
@@ -3,23 +3,53 @@
 
 # run like:
 # time for i in *.prf; do echo $i; hol2nrs $i >> 00; done
-#
+# isa2nrs.pl 00 >01
 
-<>; $name=<>; chop($name);
+# NOTES: 
+#
+# - defined symbols (constants) typically create also a definitional
+# theorem with the same name. In .prf files they can appear both as C
+# and as D. In the .prf files corresponding to definitions, we should
+# add the symbol also to @syms.
+
+# TODO:
+#
+# It seems that we also have terms in the .prf file - could be used
+# for even finer feature representation.
+
+
+my @refs = ();
+my @syms = ();
+
+my $kind = <>;
+chop($kind);
+my $name = <>;
+chop($name);
 $_=<>; die "bad" unless m/^>Inherit/;
+my $section = "Inherit";
 while(<>)
 {
-    if(m/^[>]/)
+
+    if(m/^[>](.*)/)
     {
-	print (" ", $name, ":", join(",",@refs), ";", join(",",@syms), "\n");
-	exit;
+	$section = $1;
     }
-    elsif(m/^[ABDLTS].* +(.*)/)
+
+
+    if($section =~ m/Inherit|Fetches/)
     {
-	push(@refs, $1);
-    }
-    else
-    {
-	m/^.* (.*)/ or die; push(@syms, $1)
+	if(m/^[ABDLTS].* +(.*)/)
+	{
+	    push(@refs, $1);
+	}
+	else
+	{
+	    m/^.* (.*)/ or die; push(@syms, $1)
+	}
     }
 }
+
+print (" ", $name, ":", join(",",@refs), ";", join(",",@syms), "\n");
+
+
+
